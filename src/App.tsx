63cf85aa88c9d49de7ab4e9e5474e1a5de549ac3
Osama Husam <osamahusam848@gmail.com>
2022-07-16 01:26:36 +0300

    v1.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,72 @@
+import { useState, useEffect } from "react";
+import { Table,Button } from "antd";
 function App() {
+  const [dataSource, setDataSource] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(5);
+  useEffect(() => {
+    setLoading(true);
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.json())
+      .then((data) => setDataSource(data))
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  }, []);
+  const columns: {
+    title: string;
+    dataIndex?: string;
+    key: string;
+    render?: (name: any) => JSX.Element;
+    sorter?: (a: any, b: any) => number;
+    filters?: { text: string; value: boolean }[];
+    onFilter?:any
+  }[] = [
+    {
+      title: "ID",
+      dataIndex: "id",
+      key: "1",
+    },
+    {
+      title: "User ID",
+      dataIndex: "userId",
+      key: "2",
+      sorter: (a, b) => a.userId - b.userId,
+    },
+    {
+      key: "3",
+      title: "Status",
+      dataIndex: "completed",
+      render: (completed) => <p>{completed ? "Completed" : "In Progress"}</p>,
+      filters: [
+        { text: "Completed", value: true },
+        { text: "In Progress", value: false },
+      ],
+      onFilter: (value:any, record:any) => record.completed === value,
+    },
+    {
+      key: "4",
+      title: "title",
+      dataIndex: "title",
+    },
+  ];
   return (
-    <>
-
-    </>
+    <div>
+      <Table
+        columns={columns}
+        loading={loading}
+        dataSource={dataSource}
+        pagination={{
+          current: page,
+          pageSize: pageSize,
+          onChange: (page, pageSize) => {
+            setPage(page);
+            setPageSize(pageSize);
+          },
+        }}
+      />
+      <br />
+    </div>
   );
 }
 
